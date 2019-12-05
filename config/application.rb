e95b1412d9722fc6598ba80cae794e1a35ad73e5
Andrew Shyika <andrew.shyika@gmail.com>
2019-12-05 14:52:56 +0200

    Config for gitpod.io

--- config/application.rb
+++ config/application.rb
@@ -11,9 +11,10 @@ module RailsGirlsTemplateApp
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
 
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration can go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded after loading
-    # the framework and any gems in your application.
+    # Settings needed for gitpod.io
+    config.action_view.embed_authenticity_token_in_remote_forms = true
+    config.action_dispatch.default_headers = {
+        'X-Frame-Options' => 'ALLOWALL'
+    }
   end
 end
